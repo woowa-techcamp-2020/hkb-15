7f5dab3e56f87b846aa03bf4acb0d717597c9ecf
zoomkoding <zoomkoding@gmail.com>
2020-07-27 19:53:05 +0900

    feat: add styling setting

---
+++ server/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+}
