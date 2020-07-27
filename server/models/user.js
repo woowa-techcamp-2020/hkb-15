47be618d67a3e220336c929c35fb08464a056750
zoomkoding <gur5381@gmail.com>
2020-07-28 00:31:43 +0900

    fix: update user table to have defaultWhere and correct attributes

--- server/models/user.js
+++ server/models/user.js
@@ -1,17 +1,21 @@
 const Model = require('./model')
 
 class User extends Model {
-  static defaultWhere = { isActive: 1 }
   static init() {
-    return super.init({
-      id: { dataType: 'int', required: false },
-      username: { dataType: 'varchar', required: true },
-      password: { dataType: 'varchar', required: true },
-      nickname: { dataType: 'varchar', required: true },
-      isDeleted: { dataType: 'boolean', required: false },
-      createdAt: { dataType: 'datetime', required: false },
-      updatedAt: { dataType: 'datetime', required: false },
-    })
+    return super.init(
+      {
+        id: { dataType: 'int', required: false },
+        username: { dataType: 'varchar', required: true },
+        password: { dataType: 'varchar', required: true },
+        nickname: { dataType: 'varchar', required: true },
+        isDeleted: { dataType: 'tinyint', required: false },
+        createdAt: { dataType: 'datetime', required: false },
+        updatedAt: { dataType: 'datetime', required: false },
+      },
+      {
+        defaultWhere: { isDeleted: 0 },
+      }
+    )
   }
 }
 
