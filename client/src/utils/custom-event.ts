7b1ea3f57e37da82b9b19f7b2eedf2b99b7993e0
zoomkoding <gur5381@gmail.com>
2020-07-30 02:03:18 +0900

    feat: add types to the custom events

--- client/src/utils/custom-event.ts
+++ client/src/utils/custom-event.ts
@@ -1,12 +1,15 @@
 export default {
-  fire: function (type, data) {
+  fire: function (type: string, data?: unknown): void {
     document.dispatchEvent(
       new CustomEvent(type, {
         detail: data,
       })
     )
   },
-  subscribe: function (type, listener) {
+  subscribe: function (
+    type: string,
+    listener: EventListenerOrEventListenerObject
+  ): void {
     document.addEventListener(type, listener)
   },
 }
