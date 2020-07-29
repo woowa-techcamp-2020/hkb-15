b0c513201b399edf9b8e3e4213d0b50ccd5e3736
zoomkoding <gur5381@gmail.com>
2020-07-30 02:05:19 +0900

    feat: create global types

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,7 +1,28 @@
-export type Content = Record<string, string | number | string[]>
+export type Content = Record<string, string | number | string[]> | Content[]
+export interface Category {
+  id: number
+  name: string
+  type: string
+}
+export interface Payment {
+  id: number
+  name: string
+}
+
+export interface History {
+  id: number
+  type: string
+  date: Date
+  content: string
+}
+
+export interface Store {
+  categories?: Category[]
+  payments?: Payment[]
+  histories?: History[]
+  expenditureSum?: number
+  incomeSum?: number
+}
 export interface View {
-  render(): void
-  init(data: Content): Promise<void>
-  addEventListener(): void
-  addCustomEventListener(): void
+  render(e: Event): void
 }
