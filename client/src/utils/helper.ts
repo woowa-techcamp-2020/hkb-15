37bd87632846939e01e23bb81ac83b8c4f3c8371
zoomkoding <gur5381@gmail.com>
2020-07-31 02:27:51 +0900

    feat: create a helper function that formats a date

--- client/src/utils/helper.ts
+++ client/src/utils/helper.ts
@@ -9,6 +9,7 @@ export function groupBy(objArr: {}[], key: string): object {
     return a
   }, {})
 }
+
 export const monthStr = [
   '',
   'Jan',
@@ -24,3 +25,16 @@ export const monthStr = [
   'Nov',
   'Dec',
 ]
+
+const dayStr = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+
+export function dateWithDay(dateStr: string): string {
+  const dateObj = new Date(dateStr)
+  const day = dateObj.getDay()
+  const date = dateObj.getDate()
+  let dateExtension = 'th'
+  if (date == 1) dateExtension = 'st'
+  else if (date == 2) dateExtension = 'st'
+  else if (date == 3) dateExtension = 'rd'
+  return `${dayStr[day]}, ${date}${dateExtension}`
+}
