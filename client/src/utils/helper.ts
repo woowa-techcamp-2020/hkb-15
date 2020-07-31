ed78280d902cdd832174803756f66db62c84613f
jhaemin <io@jhaemin.com>
2020-07-31 13:16:03 +0900

    dev: create a dommer function to create dom fragment from string

--- client/src/utils/helper.ts
+++ client/src/utils/helper.ts
@@ -38,3 +38,21 @@ export function dateWithDay(dateStr: string): string {
   else if (date == 3) dateExtension = 'rd'
   return `${dayStr[day]}, ${date}${dateExtension}`
 }
+
+export const dommer = (markup: TemplateStringsArray | string) => {
+  const frag = document.createDocumentFragment()
+
+  const markupStr =
+    typeof markup === 'string' ? markup : Array.isArray(markup) ? markup[0] : ''
+
+  let travelNode = new DOMParser().parseFromString(markupStr, 'text/html').body
+    .firstElementChild
+
+  while (travelNode) {
+    frag.appendChild(travelNode)
+
+    travelNode = travelNode.nextElementSibling
+  }
+
+  return frag
+}
