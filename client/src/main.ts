201561f63ce64de759317907f5093ffccca23ac8
zoomkoding <zoomkoding@gmail.com>
2020-07-31 13:46:07 +0900

    feat: move views to the router

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,19 +1,16 @@
 import 'regenerator-runtime/runtime'
 import Model from './models'
 import Router from './router'
-import HeaderView from './views/header-view'
-import HistoryView from './views/history-view'
+
 import cem from './utils/custom-event'
 
-new HeaderView()
-new HistoryView()
 new Model()
 new Router()
 
 cem.fire(
   'statechange',
   history.state ?? {
-    path: '/',
+    path: '/calendar',
     year: 2020,
     month: 7,
   }
