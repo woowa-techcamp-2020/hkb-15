d03c1f633828ced5c43ec7cf0ae0aa61aa811434
zoomkoding <gur5381@gmail.com>
2020-08-02 03:20:46 +0900

    feat: add modalView to the router

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,11 +1,13 @@
 import cem from './utils/custom-event'
 import HeaderView from './views/header-view'
 import HistoryView from './views/history-view'
+import CreateHistoryView from './views/create-history-view'
 import CalendarView from './views/calendar-view'
 export default class Router {
   constructor() {
     const headerView = new HeaderView()
     const historyView = new HistoryView()
+    const createHistoryView = new CreateHistoryView()
     const calendarView = new CalendarView()
     window.addEventListener('popstate', (event) => {
       if (event.state === null) return
