8e61ed75180718c403de94a469cda3cf0478f4e8
zoomkoding <zoomkoding@gmail.com>
2020-08-05 19:19:42 +0900

    chore: delete unused apis and rename button name

--- client/src/views/login-view/index.ts
+++ client/src/views/login-view/index.ts
@@ -10,9 +10,9 @@ export default class LoginView implements View {
   render(): void {
     const contentWrap = document.querySelector('.content-wrap')
     contentWrap.innerHTML = /*html*/ `
-      <button class="github-sign-in-button">
+      <button class="github-sign-in-button" onclick="location.href='http://localhost:3000/auth/github'">
         <i class="icon">logo_github</i>
-        Log In with Github
+        Sign In With Github
       </button>
     `
   }
