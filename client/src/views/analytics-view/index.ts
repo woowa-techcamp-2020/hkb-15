33b0a8b6785bc4845ffb982a1ebf340ab96c1f0b
jhaemin <io@jhaemin.com>
2020-08-06 01:01:02 +0900

    feat: draw line chart (#35)

--- client/src/views/analytics-view/index.ts
+++ client/src/views/analytics-view/index.ts
@@ -1,9 +1,22 @@
-import { View, History, WindowHistoryState, Category } from '../../types'
-import cem from '../../utils/custom-event'
+import type { View, History, WindowHistoryState, Category } from 'src/types'
+import cem from 'src/utils/custom-event'
 import './styles'
 import { groupBy, sum, loadHtml, html } from 'src/utils/helper'
-import { PieChart } from 'woowahan-chart'
+import { PieChart, LineChart } from 'woowahan-chart'
 import Color from 'color'
+
+const pieColorVariants = [
+  '#FF5959',
+  '#F98F54',
+  '#F0C350',
+  '#7BCA3D',
+  '#56DB9B',
+  '#2BCFDA',
+  '#5580EF',
+  '#9979F3',
+  '#D879F0',
+  '#FF5FA2',
+]
 
 export default class AnalyticsView implements View {
   state: WindowHistoryState
@@ -11,7 +24,7 @@ export default class AnalyticsView implements View {
   categories: Category[]
   sumsByCategory: {
     /** Category ID */
-    id: number
+    id: number | string
     category: string
     sum: number
     percent: number
@@ -19,25 +32,115 @@ export default class AnalyticsView implements View {
 
   constructor() {
     cem.subscribe('storeupdated', (e: CustomEvent) => {
-      if (e.detail.state.path !== '/analytics') return
+      if (e.detail.state.path !== '/analytics') {
+        document.querySelector<HTMLElement>(
+          '.sum-indicator-wrap'
+        ).style.display = ''
+        return
+      }
+
+      document.querySelector<HTMLElement>('.sum-indicator-wrap').style.display =
+        'none'
+
       this.setAttributes(e.detail)
       this.render()
     })
   }
 
+  delegateEvents(target: HTMLElement) {
+    if (
+      target.classList.contains('ao-btn') &&
+      !target.classList.contains('checked')
+    ) {
+      target.parentElement
+        .querySelectorAll('.ao-btn')
+        .forEach((btn) => btn.classList.remove('checked'))
+      target.classList.add('checked')
+
+      if (target.classList.contains('daily')) {
+        document.querySelector('section.by-categories').classList.add('hidden')
+        document.querySelector('section.daily').classList.remove('hidden')
+      } else {
+        document
+          .querySelector('section.by-categories')
+          .classList.remove('hidden')
+        document.querySelector('section.daily').classList.add('hidden')
+      }
+    }
+  }
+
   render(): void {
     const contentWrap = document.querySelector('.content-wrap')
+
     contentWrap.innerHTML = this.createBarChart()
+
+    document
+      .querySelector<HTMLElement>('.analytics-options')
+      .addEventListener('click', (e) => {
+        if (e.target instanceof HTMLElement) {
+          this.delegateEvents(e.target)
+        }
+      })
 
     if (this.sumsByCategory.length > 0) {
       PieChart({
         target: '.pie-chart',
         segments: this.sumsByCategory.map((data, i) => ({
           ...data,
-          color: Color('#f8123b')
-            .lighten(i * 0.15)
-            .string(),
+          color: pieColorVariants[i],
         })),
+      })
+
+      const groupedExpendituresHistory = groupBy(
+        this.expenditureHistories,
+        'date'
+      )
+
+      let max = 0
+      let head = 0
+      let exponent = 0
+
+      const dailyExpenditures = [
+        ...Array(new Date(this.state.year, this.state.month, 0).getDate()),
+      ]
+        .map(
+          (_, i) =>
+            `${this.state.year}-${
+              (this.state.month < 10 ? '0' : '') + this.state.month
+            }-${(i + 1 < 10 ? '0' : '') + (i + 1)}`
+        )
+        .map((date, i) => {
+          const y =
+            (groupedExpendituresHistory[date]?.reduce(
+              (accum, item) => accum + item.amount,
+              0
+            ) as number) ?? 0
+
+          if (y > max) {
+            max = y
+          }
+
+          return {
+            x: (i + 1).toString(),
+            y,
+          }
+        })
+
+      for (let i = 0; ; i += 1) {
+        if (max / 10 ** i < 1) {
+          break
+        } else {
+          head = Math.floor(max / 10 ** i)
+          exponent = i
+        }
+      }
+
+      LineChart({
+        target: '.line-chart',
+        data: dailyExpenditures,
+        maxY: head * 10 ** exponent,
+        intervalY: 10 ** exponent,
+        intervalX: 2,
       })
     }
   }
@@ -51,11 +154,11 @@ export default class AnalyticsView implements View {
     this.categories = categories
   }
 
-  calculateAmountSum(key: string): { id: number; sum: number }[] {
+  calculateAmountSum(key: string): { id: number | string; sum: number }[] {
     const groupedHistory = groupBy(this.expenditureHistories, key)
     return Object.keys(groupedHistory)
       .map((key) => ({
-        id: +key,
+        id: +key === parseInt(key) ? +key : key,
         sum: sum(groupedHistory[key], 'amount', 0),
       }))
       .sort((a, b) => b.sum - a.sum)
@@ -71,32 +174,41 @@ export default class AnalyticsView implements View {
     }))
 
     return html`
-      <div class="pie-chart"></div>
-      <div class="bar-chart">
-        ${loadHtml(
-          this.sumsByCategory.map((data) => {
-            const name = this.categories.find(
-              (category) => category.id === +data.id
-            ).name
-            const percent = ((+data.sum / totalsum) * 100).toFixed(2)
-            const amount = +data.sum
-
-            return html`
-              <div class="item-row">
-                <div class="name">
-                  ${this.categories.find((category) => category.id === data.id)
-                    .name}
-                </div>
-                <div class="percent">${percent}%</div>
-                <div class="bar-wrap">
-                  <div class="bar" style="width: ${percent}%"></div>
-                </div>
-                <div class="amount">${amount}</div>
-              </div>
-            `
-          })
-        )}
+      <div class="analytics-options">
+        <button class="ao-btn checked by-categories">By Categories</button>
+        <button class="ao-btn daily">Daily</button>
       </div>
+
+      <section class="by-categories">
+        <div class="pie-chart"></div>
+        <div class="bar-chart">
+          ${loadHtml(
+            this.sumsByCategory.map((data) => {
+              const percent = ((+data.sum / totalsum) * 100).toFixed(2)
+              const amount = +data.sum
+
+              return html`
+                <div class="item-row">
+                  <div class="name">
+                    ${this.categories.find(
+                      (category) => category.id === data.id
+                    ).name}
+                  </div>
+                  <div class="percent">${percent}%</div>
+                  <div class="bar-wrap">
+                    <div class="bar" style="width: ${percent}%"></div>
+                  </div>
+                  <div class="amount">${amount.toLocaleString()}</div>
+                </div>
+              `
+            })
+          )}
+        </div>
+      </section>
+
+      <section class="daily hidden">
+        <div class="line-chart"></div>
+      </section>
     `
   }
 }
