1036e752a9f3a2580bfe97749e09da0ec0660454
zoomkoding <gur5381@gmail.com>
2020-08-06 14:09:20 +0900

    feat: create defaultAttributes(id, createdAt, updatedAt)

--- server/models/model.js
+++ server/models/model.js
@@ -23,6 +23,12 @@ class Model {
     queueLimit: 0,
   })
 
+  static defaultAttributes = {
+    id: { dataType: DataType.int },
+    createdAt: { dataType: DataType.timestamp },
+    updatedAt: { dataType: DataType.timestamp },
+  }
+
   static validationError = createHttpError(400, 'invalid input')
 
   /**
@@ -61,7 +67,13 @@ class Model {
     const validatedInput = {}
     for (const [name, value] of Object.entries(input)) {
       console.log(name, value)
-      if (!this.attributes[name] || value === undefined) continue
+      if (
+        !this.attributes[name] ||
+        !this.defaultAttributes[name] ||
+        value === undefined
+      ) {
+        continue
+      }
       switch (this.attributes[name].dataType) {
         case 'tinyint':
           if (value == 0 || value == 1) validatedInput[name] = value
